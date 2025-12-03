5c835f23312acf108f2566731e5d541816a115d3
Rohan Das <bornforcoding777@gmail.com>
2025-12-03 12:37:56 +0530

    Created project basics, added local connection details

---
+++ src/main/java/com/rohan/inventory_stock_system/InventoryStockSystemApplication.java
@@ -0,0 +1,14 @@
+package com.rohan.inventory_stock_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryStockSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryStockSystemApplication.class, args);
+        System.out.println("Rohan");
+	}
+
+}
